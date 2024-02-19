ea0ab96da34c28fdb12b76de5ad14c47ad9903fc
tobiasvielmetter <tobias@wrklst.art>
2024-02-19 18:21:54 +0100

    combine all shell commands into single script to reduce ssh commands

--- src/Recorders/RemoteServers.php
+++ src/Recorders/RemoteServers.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace WrkLst\Pulse\RemoteServer\Recorders;
 
 use Illuminate\Config\Repository;
@@ -43,52 +44,55 @@ class RemoteServers
         $remote_ssh = $this->config->get('pulse.recorders.' . self::class . '.server_ssh');
         $server = $this->config->get('pulse.recorders.' . self::class . '.server_name');
         $slug = Str::slug($server);
-        
+
+        $dir = dirname(__FILE__);
+
+        $remoteServerStats = match (PHP_OS_FAMILY) {
+            'Darwin' => (`$remote_ssh 'bash -s' < $dir/server-stats-linux.sh`),
+            'Linux' => (`$remote_ssh 'bash -s' < $dir/server-stats-linux.sh`),
+            default => throw new RuntimeException('The pulse:check command does not currently support ' . PHP_OS_FAMILY),
+        };
+
+        $remoteServerStats = explode("\n", $remoteServerStats);
+
         /*
-        TODO
-        This needs to be optomized to reduce the amount of ssh commands fired.
-        E.g. running all commands with one ssh call with piping a shell script into ssh.
-        ´cat server-stats.sh | ssh 1.2.3.4´
+         cat /proc/meminfo | grep MemTotal | grep -E -o '[0-9]+'
+        [0] 19952552
+         cat /proc/meminfo | grep MemAvailable | grep -E -o '[0-9]+'
+        [1] 5534304
+         top -bn1 | grep -E '^(%Cpu|CPU)' | awk '{ print $2 + $4 }'
+        [2] 18
+        df --output=used,avail /
+        [3]     Used     Avail
+        [4] 34218600 473695292
         */
 
-        $memoryTotal = match (PHP_OS_FAMILY) {
-            'Darwin' => intval(`$remote_ssh 'cat /proc/meminfo' | grep MemTotal | grep -Eo '[0-9]+'` / 1024),
-            'Linux' => intval(`$remote_ssh 'cat /proc/meminfo' | grep MemTotal | grep -E -o '[0-9]+'` / 1024),
-            default => throw new RuntimeException('The pulse:check command does not currently support ' . PHP_OS_FAMILY),
-        };
+        $memoryTotal = intval($remoteServerStats[0] / 1024);
+        $memoryUsed = $memoryTotal - intval($remoteServerStats[1] / 1024);
+        $cpu = (int) $remoteServerStats[2];
 
-        $memoryUsed = match (PHP_OS_FAMILY) {
-            'Darwin' => $memoryTotal - intval(`$remote_ssh 'cat /proc/meminfo' | grep MemAvailable | grep -Eo '[0-9]+'` / 1024), // MB
-            'Linux' => $memoryTotal - intval(`$remote_ssh 'cat /proc/meminfo' | grep MemAvailable | grep -E -o '[0-9]+'` / 1024), // MB
-            default => throw new RuntimeException('The pulse:check command does not currently support ' . PHP_OS_FAMILY),
-        };
+        $storageDirectories = $this->config->get('pulse.recorders.' . self::class . '.directories');
 
-        $cpu = match (PHP_OS_FAMILY) {
-            'Darwin' => (int) `$remote_ssh 'top -bn1' | grep -E '^(%Cpu|CPU)' | awk '{ print $2 + $4 }'`,
-            'Linux' => (int) `$remote_ssh 'top -bn1' | grep -E '^(%Cpu|CPU)' | awk '{ print $2 + $4 }'`,
-            default => throw new RuntimeException('The pulse:check command does not currently support ' . PHP_OS_FAMILY),
-        };
+        if (count($storageDirectories) == 1 && $storageDirectories[0] == "/") {
+            $storage = preg_replace('/\s+/', ' ', $remoteServerStats[4]); // replace multi space with single space
+            $storage = explode(" ", $storage); // break into segments based on sigle space
 
-        $this->pulse->record('cpu', $slug, $cpu, $event->time)->avg()->onlyBuckets();
-        $this->pulse->record('memory', $slug, $memoryUsed, $event->time)->avg()->onlyBuckets();
-        $this->pulse->set('system', $slug, json_encode([
-            'name' => $server,
-            'cpu' => $cpu,
-            'memory_used' => $memoryUsed,
-            'memory_total' => $memoryTotal,
-            'storage' => collect($this->config->get('pulse.recorders.' . self::class . '.directories')) // @phpstan-ignore argument.templateType argument.templateType
+            $storageTotal = $storage[0] + $storage[1]; // used and availble
+            $storageUsed = $storage[0]; // used
+
+            $storage = [collect([
+                'directory' => "/",
+                'total' => intval(round($storageTotal / 1024)), // MB
+                'used' => intval(round($storageUsed / 1024)), // MB
+            ])];
+        } else {
+            $storage = collect($storageDirectories)
                 ->map(function (string $directory) use ($remote_ssh) {
                     $storage = match (PHP_OS_FAMILY) {
                         'Darwin' => (`$remote_ssh 'df $directory'`),
                         'Linux' => (`$remote_ssh 'df $directory'`),
                         default => throw new RuntimeException('The pulse:check command does not currently support ' . PHP_OS_FAMILY),
                     };
-
-                    /*
-                    // Example output
-                    Filesystem     1K-blocks     Used Available Use% Mounted on
-                    /dev/root      507930276 31400452 476513440   7% /
-                    */
 
                     $storage = explode("\n", $storage); // break in lines
                     $storage = preg_replace('/\s+/', ' ', $storage[1]); // replace multi space with single space
@@ -103,7 +107,17 @@ class RemoteServers
                         'used' => intval(round($storageUsed / 1024)), // MB
                     ];
                 })
-                ->all(),
+                ->all();
+        }
+
+        $this->pulse->record('cpu', $slug, $cpu, $event->time)->avg()->onlyBuckets();
+        $this->pulse->record('memory', $slug, $memoryUsed, $event->time)->avg()->onlyBuckets();
+        $this->pulse->set('system', $slug, json_encode([
+            'name' => $server,
+            'cpu' => $cpu,
+            'memory_used' => $memoryUsed,
+            'memory_total' => $memoryTotal,
+            'storage' => $storage,
         ], flags: JSON_THROW_ON_ERROR), $event->time);
     }
 }
